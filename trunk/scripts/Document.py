b10ee72a59a86ff67fd86799bb3eb1c121bf1ab9
irms19@gmail.com <irms19@gmail.com@c0292a02-c6b8-4545-276b-13d5b17a225e>
2011-11-24 20:29:32 +0000

    Calculates safety scores for all APIs for some countries. Need to work on all the countries (WIP)

--- trunk/scripts/Document.py
+++ trunk/scripts/Document.py
@@ -1,13 +1,16 @@
-#keywords = {"tornado": 3, "war":2, "quake":4, "earthquake":4}  # for testing purposes
+import re
 
+keywords = {"tornado": 0.5, "earthquake": 0.7, "quake": 0.7, "civil war": 0.5, "travelalert": 0.9, "emergency": 0.9, "travelwarning": 0.9} #testing purposes
+
+# Creates a Document object that finds and keeps the frequency 
+# of keywords in that document
 class Document:
-    #
-    #
-    # parameter:
+    # parameters:
     #   link - string with url of article
-    #   news - list of words
+    #   news - string of title and body
+    #   frequencies - dictionary containing all the frequencies in all the documents
     #   keywords - dictionary with keywords and scores 
-    def __init__(self, link, news, frequencies, keywords):    #title, body):
+    def __init__(self, link, news, frequencies, keywords):    
         self.keywords = keywords
         self.link = link
         self.total_words = 0
@@ -19,22 +22,35 @@ class Document:
     # purpose: find keywords in text and keep the count
     # preconditions:
     # parameters:
-    #       text - list of words
+    #       text - string to analyze
+    #       frequencies - dictionary for the entire query {"keyword": frequency}
     # returns:
     #       tuple - (dictionary of keywords, total word count)
     def countWords(self, text, frequencies):
         d = {}
-        self.total_words = len(text)
-        for word in text:
-            if word in d:                 # word is a keyword and add the weight of that word
-                d[word] += self.keywords[word]
-            else:
-                if word in self.keywords:                # word in keywords
-                    d[word] = self.keywords[word]
-                    if word in frequencies:         # frequency added the first time the keyword is found
-                        frequencies[word] += 1
-                    else:
-                        frequencies[word] = 1
-        
-       # print "\n\nDictionary of words", d
+        self.total_words = len(text.split())
+        for keyword in self.keywords:                           # finds all the keywords present in the text
+            match = re.compile(keyword)
+            allMatches = match.findall(text)
+            if len(allMatches):                                 # keyword in text
+                freq = len(allMatches)                          # frequency of keyword in text
+                d[keyword] = freq * self.keywords[keyword]      # calculates the weight * freq of keyword
+                if keyword in frequencies:
+                    frequencies[keyword] += 1
+                else:
+                    frequencies[keyword] = 1
+
         return d
+
+
+def main(args):
+    link = "link1"
+    news = 'travelwarning civil emergency war war tornado quake ECONOMIC MEMO; The Gridlock Where Debts Meet Politics - Economic Memo WASHINGTON &mdash; With Greece struggling to form a government that can force harsh austerity measures onto a weary public, Europe is in usual form, taking a couple of steps toward solving its fiscal crisis and then a couple of steps backward. Washington, meanwhile, is hoping that the latest deficit-reduction committee in Congress can succeed where tornado war quake'
+    frequencies = {}
+    doc = Document(link, news, frequencies, keywords)
+    print "score: ", doc.scores, "frequencies: ", frequencies
+    
+   
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)    
