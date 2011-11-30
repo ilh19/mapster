38b9e8d98216295bfd44a7256895667695476f55
irms19@gmail.com <irms19@gmail.com@c0292a02-c6b8-4545-276b-13d5b17a225e>
2011-11-30 14:58:42 +0000

    Modify index under mapster-GAE, is ready to be deployed to Google App Engine.

--- trunk/mapster/scripts/mainpage.py
+++ trunk/mapster/scripts/mainpage.py
@@ -1,3 +1,22 @@
-print 'Content-Type: text/plain'
-print ''
-print 'Hello, world!'
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        guestbook_name=self.request.get('guestbook_name')
+        greetings_query = Greeting.all().ancestor(
+            guestbook_key(guestbook_name)).order('-date')
+        greetings = greetings_query.fetch(10)
+
+        if users.get_current_user():
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        template_values = {
+            'greetings': greetings,
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render(template_values))
