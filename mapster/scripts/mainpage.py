ca190cea90ed9834053408c93cc8664cce304fbb
lmdunn2012@gmail.com <lmdunn2012@gmail.com@c0292a02-c6b8-4545-276b-13d5b17a225e>
2011-11-30 22:49:05 +0000

    Updating web branch (in old spot)

--- mapster/scripts/mainpage.py
+++ mapster/scripts/mainpage.py
@@ -1,22 +1,62 @@
-class MainPage(webapp2.RequestHandler):
+
+import os
+import cgi
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+
+class MainPage(webapp.RequestHandler):
     def get(self):
-        guestbook_name=self.request.get('guestbook_name')
-        greetings_query = Greeting.all().ancestor(
-            guestbook_key(guestbook_name)).order('-date')
-        greetings = greetings_query.fetch(10)
-
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
+        is_result = True
+        result_string = 'fff'
+        """try:
+            result_string = cgi.escape(self.request.get('map-radio'))
+        except:
+            is_result = False
+        
+        map_url = ''
+        if not is_result:
+            map_url = self.get_map('All')
         else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
+            map_url = self.get_map(result_string)
+            
+        path = os.path.split(__file__)
+        path = os.path.split(path[0])
+        path = os.path.join(path[0], 'web/style.css')
+        
         template_values = {
-            'greetings': greetings,
-            'url': url,
-            'url_linktext': url_linktext,
-        }
+            'map_url' : map_url,
+        }"""
+        path = os.path.join(os.path.dirname(__file__), '../web/index.html')
+        self.response.out.write(template.render(path, {}))
+        
+    def get_map(self, result):
+        if result == 'Nuclear':
+            return '"http://www.google.com/fusiontables/embedviz?viz=MAP&q=select+col1%3E%3E1+from+2301384+&h=false&lat=0&lng=0&z=3&t=1&l=col1%3E%3E1"'
+        elif result == 'Twitter':
+            return '"http://www.google.com/fusiontables/embedviz?viz=MAP&q=select+col1%3E%3E1+from+2301764+&h=false&lat=0&lng=0&z=3&t=1&l=col1%3E%3E1"'
+        elif result == 'Epidemic':
+            return '"http://www.google.com/fusiontables/embedviz?viz=MAP&q=select+col1%3E%3E1+from+2301395+&h=false&lat=0&lng=0&z=3&t=1&l=col1%3E%3E1"'
+        elif result == 'Politics':
+            return '"http://www.google.com/fusiontables/embedviz?viz=MAP&q=select+col1%3E%3E1+from+2301389+&h=false&lat=0&lng=0&z=3&t=1&l=col1%3E%3E1"'
+        elif result == 'Crime':
+            return '"http://www.google.com/fusiontables/embedviz?viz=MAP&q=select+col1%3E%3E1+from+2301765+&h=false&lat=0&lng=0&z=3&t=1&l=col1%3E%3E1"'
+        elif result == 'Other':
+            return '"http://www.google.com/fusiontables/embedviz?viz=MAP&q=select+col1%3E%3E1+from+2301499+&h=false&lat=0&lng=0&z=3&t=1&l=col1%3E%3E1"'
+        elif result == 'NaturalDisasters':
+            return '"http://www.google.com/fusiontables/embedviz?viz=MAP&q=select+col1%3E%3E1+from+2301375+&h=false&lat=0&lng=0&z=3&t=1&l=col1%3E%3E1"'
+        else:
+            #all
+            return '"http://www.google.com/fusiontables/embedviz?viz=MAP&q=select+col1%3E%3E1+from+2301774+&h=false&lat=0&lng=0&z=3&t=1&l=col1%3E%3E1"'
+        
 
-        template = jinja_environment.get_template('index.html')
-        self.response.out.write(template.render(template_values))
+application = webapp.WSGIApplication(
+                                     [('/', MainPage),
+                                     ('/index', MainPage)],
+                                     debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
